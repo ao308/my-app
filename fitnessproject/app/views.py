0372e6e086b7972c3797bc367640a9540fd1efd0
ao308 <122pen.nizi@gmail.com>
2025-10-11 16:49:13 +0900

    ポートフォリオ

--- fitnessproject/app/views.py
+++ fitnessproject/app/views.py
@@ -5,9 +5,4 @@ from .models import Profile
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        profile_data = Profile.objects.all()
-        if profile_data.exists():
-            profile_data = profile_data.order_by('-id')[0]
-            return render(request, 'app/index.html', {
-                'profile_data' : profile_data
-            })
+        return render(request, 'app/index.html')
